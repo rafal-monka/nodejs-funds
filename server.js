5901b320dccd1ea81ee85a68ea6c11ffb3bb38bd
Rafal Monka <monka.rafal@gmail.com>
2020-08-08 18:44:28 +0200

    init prod

--- server.js
+++ server.js
@@ -4,20 +4,15 @@
 require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require('path')
 
 // const mongodb = require("./app/mongodb.js");
 const initDatabase = require('./config/database')
 const schedule = require('node-schedule')
 
-
 const mojefundusze = require("./app/mojefundusze-crawler.js");
 const observations = require("./app/controllers/observations.js");
 const status = require("./app/status.js");
-    // const db = require("./app/models");
-
-    //@@@development = true; production = false
-    // db.sequelize.sync(  { force: false }  ); //!!! In development, you may need to drop existing tables and re-sync database.
-
 
 const app = express();
 
@@ -26,8 +21,22 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
     res.json({ message: "Welcome to Funds application." });
+});
+
+
+//make default URL for SPA
+const buildLocation = 'public'; //include public folder with SPA app
+app.use(express.static(path.join(__dirname, buildLocation)));
+
+//#https://blog.fullstacktraining.com/404-after-refreshing-the-browser-for-angular-vue-js-app/
+app.use("/*", (req, res, next) => {
+    if (!req.originalUrl.includes(buildLocation) && !req.originalUrl.includes('/api/') && !req.originalUrl.includes('/login') && !req.originalUrl.includes('/logout') ) {    
+        res.sendFile(`${__dirname}/${buildLocation}/index.html`);
+    } else {
+        next();
+    }
 });
 
 //App API endpoints
