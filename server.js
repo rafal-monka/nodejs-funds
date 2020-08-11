329b0a04c16a9d4d1fd97a98d1295f566767962a
Rafal Monka <monka.rafal@gmail.com>
2020-08-11 17:05:35 +0200

    cronParams = "0 0,15,30,45 14-16 * * 1-6"

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ mojefundusze.perform()
 
 
 //schedule word reminder
-cronParams = "0 15 13-16 * * 1-6"
+cronParams = "0 0,15,30,45 14-16 * * 1-6"
 console.log('schedule', new Date(), cronParams)
 
 var j = schedule.scheduleJob(cronParams, function(){ 
