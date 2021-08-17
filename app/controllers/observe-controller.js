ed5fb43168f41aea549f913ffcdc23258b4ab1d5
Rafal Monka <monka.rafal@gmail.com>
2021-08-17 17:54:06 +0200

    Observe Kruggerand v.beta2

--- app/controllers/observe-controller.js
+++ app/controllers/observe-controller.js
@@ -9,9 +9,13 @@ exports.observeKrugerrand = (req, res, next) => {
         url: url
     }
     getInformation(request).then(result => {
-        res.json(
-            processInformation(result.data)
-        )
+        let price = processInformation(result.data)
+        let message = (price < 7100 ? 'BUY': 'WAIT')
+        email.sendEmail(
+            ' Funds (Observe Krugerrand) '+message+': '+price+' zł, date:'+new Date().toISOString(), 
+            '<div><pre>'+price+'zł </pre></div>'
+        ) 
+        res.json(price)
     })
     
 }
@@ -36,7 +40,7 @@ processInformation = (html) => {
     let result = null 
 	try {
         let projector_price_value = dom.getElementById('projector_price_value')
-        result = projector_price_value.innerHTML.replace(' ','').replace(' zł','')
+        result = projector_price_value.innerHTML.replace(' ','').replace(' zł','').replace(',','.')
         console.log(result)
         return result
     } catch (e) {
